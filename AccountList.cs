793c6764bc21a6893973b92b570bf65d7aace21a
minht <minht@MINBOOK>
2024-03-15 10:28:50 +0700

    Cn đăng nhập thành công

---
+++ AccountList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KtGitHub
+{
+    public class AccountList
+    {
+        private static AccountList instance;
+
+        public static AccountList Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new AccountList();
+                return instance;
+            }
+            set => instance = value;
+        }
+
+        List<Account> listAccount;
+
+        public List<Account> ListAccount
+        {
+            get => listAccount;
+            set => listAccount = value;
+        }
+
+
+        AccountList()
+        {
+            listAccount = new List<Account>();
+            listAccount.Add(new Account("admin", "admin"));
+        }
+    }
+}
---
+++ FormMain.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace KtGitHub
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(1434, 743);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(6);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(240, 77);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(1094, 743);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(6);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(240, 77);
+            this.btnLogout.TabIndex = 2;
+            this.btnLogout.Text = "Log Out";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1747, 883);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogout);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
